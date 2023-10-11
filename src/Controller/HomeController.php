f3d355052ffdd0cb1b6cc2232d11257aec7ceea3
FoxDeFacto <lisakzezhore@seznam.cz>
2023-10-11 18:11:19 +0200

    Add page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,12 +16,12 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/info', name: 'app_home')]
-    public function info(): Response
+    #[Route('/info', name: 'app_home')] //Cesta s názvem -> '/info'  // Název pro cestu kontroler 'app_home'
+    public function info(): Response //Název metody
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/info.html.twig', [ //Cesta jakopu stránku má kontroler vykreslit
             'controller_name' => 'HomeController',
-            'variable_test' => 'Value_test',
+            'variable_test' => 'Value_test', //Název proměné a její hodnota
         ]);
     }
 }
