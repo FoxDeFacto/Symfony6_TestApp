8b5e941e5a75611afeb15067909b59a069000d3b
FoxDeFacto <lisakzezhore@seznam.cz>
2023-10-11 17:54:23 +0200

    Initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/info', name: 'app_home')]
+    public function info(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'variable_test' => 'Value_test',
+        ]);
+    }
+}
