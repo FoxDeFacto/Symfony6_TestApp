311388f55483062f9ec8d3aca352b9815583dc0c
FoxDeFacto <lisakzezhore@seznam.cz>
2023-10-11 18:56:35 +0200

    Database setup

---
+++ src/Entity/Product.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(targetEntity: ProductType::class)] //Každý produkt má jeden typ
+    #[ORM\JoinColumn(name: "type", referencedColumnName: "id")]
+    private ?ProductType $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?ProductType
+    {
+        return $this->type;
+    }
+
+    public function setType(ProductType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
