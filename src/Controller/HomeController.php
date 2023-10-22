9ef28a9320581658e06b52cf1724c8f3c8509f92
FoxDeFacto <lisakzezhore@seznam.cz>
2023-10-22 16:27:04 +0200

    CRUD operations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,40 +14,41 @@ use Symfony\Component\HttpFoundation\Request; //Přidáno pro zpracování dat p
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(): Response
     {
 
-         // najít všechny produkty
-         $products = $doctrine->getManager()->getRepository(Product::class)->findAll();
-
-         $productTypes = $doctrine->getRepository(ProductType::class)->findAll();
-
-         foreach ($products as $product) // Spojí ProductType s Product na základě cizího klíče v Product
-         {
-            $type = $product->getProductType();
-            foreach ($productTypes as $productType) 
-            {
-                if ($type->getId() == $productType->getId()) 
-                {
-                    $product->setProductType($productType);
-                    break;
-                }
-            }
-        }
-        
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'products' => $products
-        ]);
-    }
-
-    #[Route('/info', name: 'app_info')] //Cesta s názvem -> '/info'  // Název pro cestu kontroler 'app_home'
-    public function info(): Response //Název metody
-    {
-        return $this->render('home/info.html.twig', [ //Cesta jakopu stránku má kontroler vykreslit
+        return $this->render('home/index.html.twig', [ //Cesta jakopu stránku má kontroler vykreslit
             'controller_name' => 'HomeController',
             'variable_test' => 'Value_test', //Název proměné a její hodnota
         ]);
+    }
+
+    #[Route('/overview', name: 'app_overview', methods: ['GET'])] //Cesta s názvem -> '/info'  // Název pro cestu kontroler 'app_home'
+    public function overview(ManagerRegistry $doctrine): Response //Název metody
+    {
+
+        // najít všechny produkty
+        $products = $doctrine->getManager()->getRepository(Product::class)->findAll();
+
+        $productTypes = $doctrine->getRepository(ProductType::class)->findAll();
+
+        foreach ($products as $product) // Spojí ProductType s Product na základě cizího klíče v Product
+        {
+           $type = $product->getProductType();
+           foreach ($productTypes as $productType) 
+           {
+               if ($type->getId() == $productType->getId()) 
+               {
+                   $product->setProductType($productType);
+                   break;
+               }
+           }
+       }
+       
+       return $this->render('home/overview.html.twig', [
+           'controller_name' => 'HomeController',
+           'products' => $products
+       ]);
     }
 
     #[Route('/product/new', name: 'product_new')] //cesta 
@@ -68,9 +69,9 @@ class HomeController extends AbstractController
  
             $product = $form->getData();
 
-            $em = $doctrine->getManager();
+            $em = $doctrine->getManager(); // Objekt pro práci s entitami
 
-            $em->persist($product);
+            $em->persist($product); //Přidání do fronty vykonání v databázi - teoreticky zbytečné pro tento případ
             $em->flush(); //Přidání data do databáze
 
             return $this->redirect('/'); //Přesměrování na Homepage
@@ -78,6 +79,90 @@ class HomeController extends AbstractController
 
         return $this->render('home/product_new.html.twig', [ //Stránka s daným formulářem
             'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/product/edit/{id}', name: 'product_edit')]
+    public function edit(Request $request, ManagerRegistry $doctrine, $id): Response //Přidá parametr id pro zjištění záznamu
+    {
+        $product = $doctrine->getManager()->getRepository(Product::class)->find($id); //Najde na základě id záznam
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id ' . $id); //Error pokud není záznam nalezen
+        }
+
+        $form = $this->createForm(ProductFormType::class, $product);  //Tvorba nového formuléře podle vzoru ProductFormType
+        
+        $form->handleRequest($request);  //Předání dat z formuláře
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $em = $doctrine->getManager(); // Objekt pro práci s entitami
+            
+            $em->flush(); // Provedení změn vdatabázi
+            
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/product_edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/product/delete/{id}', name: 'product_delete')]
+    public function delete(Request $request, ManagerRegistry $doctrine, $id): Response
+    {
+        $product = $doctrine->getManager()->getRepository(Product::class)->find($id); //Najde na základě id záznam
+        
+        if (!$product) 
+        {
+            throw $this->createNotFoundException('No product found for id ' . $id); //Error pokud není záznam nalezen
+        }
+
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) 
+        {
+                $em = $doctrine->getManager();
+                $em->remove($product);
+                $em->flush();
+
+                return $this->render('home/index.html.twig', [ //Cesta jakopu stránku má kontroler vykreslit
+                    'controller_name' => 'HomeController',
+                    'variable_test' => 'Value_test', //Název proměné a její hodnota
+                ]);
+        }
+        
+
+        return $this->render('home/product_delete.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+
+    #[Route('/product/read/{id}', name: 'product_read')]
+    public function read(ManagerRegistry $doctrine, $id): Response
+    {
+        $product = $doctrine->getManager()->getRepository(Product::class)->find($id); //Najde na základě id záznam
+
+        $productTypes = $doctrine->getRepository(ProductType::class)->findAll();
+
+        $type = $product->getProductType();
+        foreach ($productTypes as $productType) 
+        {
+            if ($type->getId() == $productType->getId()) 
+            {
+                $product->setProductType($productType);
+                break;
+            }
+        }
+
+        dump($product);
+        
+        if (!$product) 
+        {
+            throw $this->createNotFoundException('No product found for id ' . $id); //Error pokud není záznam nalezen
+        }
+
+        return $this->render('home/product_detail.html.twig', [
+            'product' => $product,
         ]);
     }
 
